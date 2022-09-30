e5735251d7b04402850cd610be8352a6dbfc18d9
Aygran <95911512+Aygran@users.noreply.github.com>
2022-09-30 12:33:50 -0600

    First Push

---
+++ DungeonsAndNotes/About.xaml.cs
@@ -0,0 +1,14 @@
+namespace DungeonsAndNotes;
+
+public partial class About : ContentPage
+{
+	public About()
+	{
+		InitializeComponent();
+	}
+
+	private async void LearnMore_Clicked(object sender, EventArgs e)
+	{
+		await Launcher.Default.OpenAsync("https://aka.ms/maui");
+	}
+}
---
+++ DungeonsAndNotes/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DungeonsAndNotes;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
